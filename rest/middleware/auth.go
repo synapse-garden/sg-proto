707234db6a329dc763ec499e3472c53c10a2919e
Bodie Solomon <bsolomon@freewheel.tv>
2016-08-31 10:18:55 -0400

    Add rest auth middleware

---
+++ rest/middleware/auth.go
@@ -0,0 +1,128 @@
+package middleware
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/synapse-garden/sg-proto/auth"
+	"github.com/synapse-garden/sg-proto/store"
+
+	"github.com/boltdb/bolt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/pkg/errors"
+)
+
+type Header string
+
+const (
+	AuthHeader    Header = "Authorization"
+	RefreshHeader Header = "X-Auth-Refresh"
+)
+
+func GetToken(kind, from string) ([]byte, error) {
+	// Token is expected to be base64 encoded byte slice.
+	// Kind is assumed to be valid.
+	substrings := strings.SplitN(from, kind+" ", 2)
+	switch {
+	case len(from) == 0:
+		return nil, errors.Errorf(
+			"no %q token provided in header %q",
+			kind, AuthHeader)
+	case len(substrings) != 2:
+		return nil, errors.Errorf(
+			"invalid %q token provided in header %q",
+			kind, AuthHeader)
+	case substrings[0] != "":
+		return nil, errors.Errorf(
+			"invalid %q token kind, use %q",
+			AuthHeader, kind)
+	}
+
+	bs, err := base64.StdEncoding.DecodeString(substrings[1])
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf(
+			"failed to decode %q token", kind))
+	}
+
+	return auth.Token(bs), nil
+}
+
+func Auth(h httprouter.Handle, db *bolt.DB) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		// Is an authorized key in the header?
+		token, err := GetToken(
+			"Bearer",
+			r.Header.Get(string(AuthHeader)),
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// TODO: Split me into my own function.
+		// Check whether the session token is valid
+		err = db.View(auth.CheckToken(token))
+		if err == nil {
+			h(w, r, ps)
+			return
+		}
+
+		switch err.(type) {
+		case auth.ErrMissingSession:
+			http.Error(w, "invalid session token", http.StatusUnauthorized)
+			return
+		case auth.ErrTokenExpired:
+			rToken, err := GetToken(
+				"Refresh",
+				r.Header.Get(string(RefreshHeader)),
+			)
+			if err != nil {
+				http.Error(w, errors.Wrap(err, "invalid refresh token").Error(), http.StatusUnauthorized)
+				return
+			}
+			if err := db.View(auth.CheckRefresh(rToken)); store.IsMissing(err) {
+				http.Error(w, errors.Wrap(err, "invalid refresh token").Error(), http.StatusUnauthorized)
+				return
+			} else if err != nil {
+				http.Error(w, errors.Wrap(err, "failed to check refresh token").Error(), http.StatusInternalServerError)
+				log.Printf("Failed to check refresh token: %#v", err)
+				return
+			}
+			sess := &auth.Session{Token: token, RefreshToken: rToken}
+			err = db.Update(auth.RefreshIfValid(sess, time.Now().Add(auth.Expiration), auth.Expiration))
+			if err != nil {
+				switch err.(type) {
+				case auth.ErrMissingSession:
+					http.Error(w, "invalid refresh token", http.StatusUnauthorized)
+				default:
+					http.Error(w, errors.Wrap(err, "failed to refresh session").Error(), http.StatusInternalServerError)
+					log.Printf("Failed to refresh session: %#v", err)
+				}
+				return
+			}
+
+			if err := db.View(auth.CheckToken(token)); err != nil {
+				switch err.(type) {
+				case auth.ErrMissingSession:
+					http.Error(w, "invalid session token", http.StatusUnauthorized)
+					// Something weird happened.
+					log.Printf("Failed to verify session token after refresh: %#v", err)
+					return
+				default:
+					http.Error(w, errors.Wrap(err, "failed to verify session token after refresh").Error(), http.StatusInternalServerError)
+					log.Printf("Failed to verify session token after refresh: %#v", err)
+					return
+				}
+			}
+			// If we ran the gamut of possible errors here, we're in the clear and the session was refreshed.  Carry on.
+		default:
+			http.Error(w, errors.Wrap(err, "unexpected server error").Error(), http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
---
+++ rest/middleware/auth_test.go
@@ -0,0 +1,111 @@
+package middleware_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	htt "net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/synapse-garden/sg-proto/auth"
+	"github.com/synapse-garden/sg-proto/rest/middleware"
+	"github.com/synapse-garden/sg-proto/store"
+	sgt "github.com/synapse-garden/sg-proto/testing"
+
+	"github.com/boltdb/bolt"
+	"github.com/julienschmidt/httprouter"
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type MiddlewareSuite struct {
+	db     *bolt.DB
+	tmpDir string
+}
+
+var _ = Suite(&MiddlewareSuite{})
+
+func (s *MiddlewareSuite) SetUpTest(c *C) {
+	db, tmpDir, err := sgt.TempDB("sg-test")
+	c.Assert(err, IsNil)
+	c.Assert(db.Update(store.Wrap(
+		store.Migrate(store.Version),
+		store.SetupBuckets(
+			auth.SessionBucket,
+			auth.RefreshBucket,
+		),
+	)), IsNil)
+	s.db, s.tmpDir = db, tmpDir
+}
+
+func (s *MiddlewareSuite) TearDownTest(c *C) {
+	if db := s.db; db != nil {
+		c.Assert(sgt.CleanupDB(db), IsNil)
+		c.Assert(os.Remove(s.tmpDir), IsNil)
+	}
+}
+
+func (s *MiddlewareSuite) TestAuth(c *C) {
+	sess := &auth.Session{}
+	c.Assert(s.db.Update(auth.NewSession(
+		sess,
+		time.Now().Add(1*time.Hour),
+		time.Hour,
+		auth.NewToken(auth.BearerType),
+		auth.NewToken(auth.RefreshType),
+	)), IsNil)
+
+	validToken := base64.StdEncoding.EncodeToString(sess.Token)
+
+	r := htt.NewRequest("GET", "/foo", nil)
+	r.Header.Set(string(middleware.AuthHeader), fmt.Sprintf("%s %s", auth.BearerType, validToken))
+	h := func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Write([]byte("ok"))
+	}
+	w := htt.NewRecorder()
+	middleware.Auth(h, s.db)(w, r, nil)
+	c.Check(w.Body.String(), Equals, "ok")
+	// A request to an endpoint with the given auth scheme should
+	// be rejected if header["Authorization"] is not "Bearer" and
+	// present in the database.
+}
+
+func (s *MiddlewareSuite) TestGetToken(c *C) {
+	validToken := auth.NewToken(auth.BearerType)
+	validBase64 := base64.StdEncoding.EncodeToString(validToken)
+	c.Logf("valid token: %+v", validToken)
+	c.Logf("base64: %s", validBase64)
+
+	for i, test := range []struct {
+		given     string
+		expect    []byte
+		expectErr string
+	}{{
+		given: "",
+		expectErr: `no "Bearer" token provided in header ` +
+			`"Authorization"`,
+	}, {
+		given: "Basic 12345",
+		expectErr: `invalid "Bearer" token provided in ` +
+			`header "Authorization"`,
+	}, {
+		given: "Bearer 12345",
+		expectErr: `failed to decode "Bearer" token: illegal ` +
+			`base64 data at input byte 4`,
+	}, {
+		given:  "Bearer " + validBase64,
+		expect: validToken,
+	}} {
+		c.Logf("test %d: %s", i, test.given)
+		got, err := middleware.GetToken("Bearer", test.given)
+		if test.expectErr != "" {
+			c.Check(err, ErrorMatches, test.expectErr)
+			continue
+		}
+		c.Assert(err, IsNil)
+		c.Check(got, DeepEquals, test.expect)
+	}
+}
