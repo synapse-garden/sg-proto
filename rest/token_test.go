ab9f6954b35de149c07883fd0732ebfcad649d84
Bodie Solomon <bsolomon@freewheel.tv>
2016-09-01 09:18:11 -0400

    Add stub /tokens test

---
+++ rest/token_test.go
@@ -0,0 +1,22 @@
+package rest_test
+
+import (
+	// "github.com/synapse-garden/sg-proto/incept"
+	// "github.com/synapse-garden/sg-proto/users"
+
+	// "github.com/boltdb/bolt"
+	. "gopkg.in/check.v1"
+)
+
+func (s *RESTSuite) TestToken(c *C) {
+	// c.Assert(s.db.Update(func(tx *bolt.Tx) error {
+	// 	return incept.Incept()(tx)
+	// }), IsNil)
+	// for i, test := range []struct {
+	// 	givenUser *users.User
+	// 	expectErr string
+	// }{{}} {
+	// 	c.Logf("test %d")
+	// 	r := htt.NewRecorder()
+	// }
+}
