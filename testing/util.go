82921804035044a2806dc5fc579ca8b0290add36
Bodie Solomon <bsolomon@freewheel.tv>
2016-10-04 10:25:52 -0400

    Add timeouts to testing package

---
+++ testing/util.go
@@ -0,0 +1,10 @@
+package testing
+
+import "time"
+
+// Timeout constants.
+const (
+	ShortWait    = 20 * time.Millisecond
+	LongWait     = 300 * time.Millisecond
+	VeryLongWait = 3 * time.Second
+)
