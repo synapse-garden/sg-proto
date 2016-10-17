da7e5c6d47aeea48472130ac83ee8880709362a3
Bodie Solomon <bsolomon@freewheel.tv>
2016-10-17 08:46:37 -0400

    Add and test notif Encode

---
+++ notif/encode.go
@@ -0,0 +1,43 @@
+package notif
+
+import (
+	js "encoding/json"
+
+	"github.com/synapse-garden/sg-proto/store"
+	"github.com/synapse-garden/sg-proto/stream"
+)
+
+// Encoder is a type implementing an Encode to a River.
+// TODO: Offer Protobuf / Msgpack Encoder
+// TODO: Offer pb / json / mpack Decoder / Translator.
+type Encoder interface {
+	Encode(stream.PubRiver, store.Resourcer, stream.Topic) error
+}
+
+type json struct{}
+
+// DefaultEncoder implements a shortcut for JSON encoding to a River on
+// a Topic.
+var DefaultEncoder = json{}
+
+// Encode implements Encoder on json for DefaultEncoder.
+func (json) Encode(r stream.PubRiver, val store.Resourcer, t UserTopic) error {
+	bs, err := js.Marshal(val)
+	if err != nil {
+		return err
+	}
+	boxBs, err := js.Marshal(store.ResourceBox{
+		Name:     val.Resource(),
+		Contents: bs,
+	})
+	if err != nil {
+		return err
+	}
+	return r.Send(stream.BytesFor(t, boxBs))
+}
+
+// Encode uses the default (JSON) Encoder to send the given value on r,
+// prefixed with t.
+func Encode(r stream.PubRiver, val store.Resourcer, t UserTopic) error {
+	return DefaultEncoder.Encode(r, val, t)
+}
---
+++ notif/encode_test.go
@@ -0,0 +1,71 @@
+package notif_test
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/synapse-garden/sg-proto/notif"
+	"github.com/synapse-garden/sg-proto/store"
+	"github.com/synapse-garden/sg-proto/stream"
+
+	. "gopkg.in/check.v1"
+)
+
+type testRiver struct {
+	sends [][]byte
+}
+
+func (t *testRiver) Send(bs []byte) error {
+	t.sends = append(t.sends, bs)
+	return nil
+}
+
+func (*testRiver) Close() error { return nil }
+
+var _ = stream.PubRiver(&testRiver{})
+
+type testResourcer struct {
+	X int `json:"x"`
+}
+
+func (testResourcer) Resource() store.Resource {
+	return store.Resource("test")
+}
+
+type badResourcer map[int]int
+
+func (badResourcer) Resource() store.Resource { return store.Resource("bad") }
+
+func (badResourcer) MarshalJSON() ([]byte, error) { return nil, errors.New("oops") }
+
+func (s *NotifSuite) TestDefaultEncoderEncode(c *C) {
+	t := &testRiver{}
+	top := notif.MakeUserTopic("bob")
+	v := testResourcer{}
+
+	err := notif.DefaultEncoder.Encode(t, v, top)
+	c.Assert(err, IsNil)
+
+	vbs, err := json.Marshal(v)
+	c.Assert(err, IsNil)
+	jsbs, err := json.Marshal(&store.ResourceBox{
+		Name:     v.Resource(),
+		Contents: vbs,
+	})
+	c.Assert(err, IsNil)
+
+	c.Check(t.sends[0], DeepEquals, append(top.Code(), jsbs...))
+}
+
+func (s *NotifSuite) TestEncode(c *C) {
+	t := &testRiver{}
+	top := notif.MakeUserTopic("bob")
+	v := testResourcer{}
+
+	c.Assert(notif.DefaultEncoder.Encode(t, v, top), IsNil)
+	c.Assert(notif.Encode(t, v, top), IsNil)
+
+	c.Check(t.sends[0], DeepEquals, t.sends[1])
+
+	c.Check(notif.Encode(t, badResourcer{}, top), ErrorMatches, "json: .*oops")
+}
