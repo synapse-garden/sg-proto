ae7592a887e504a60c710172ec3ca105a89b6577
Bodie Solomon <bsolomon@freewheel.tv>
2016-10-17 08:43:36 -0400

    Add store.Resource / store.Resourcer

---
+++ store/resource.go
@@ -0,0 +1,16 @@
+package store
+
+// Resource can be used for package constants, etc. to name a resource.
+type Resource string
+
+// Resourcer is something which has a Resource.
+type Resourcer interface {
+	Resource() Resource
+}
+
+// ResourceBox is a container for the bytes and type of some encoded
+// Resource.
+type ResourceBox struct {
+	Name     Resource `json:"name"`
+	Contents []byte   `json:"contents"`
+}
