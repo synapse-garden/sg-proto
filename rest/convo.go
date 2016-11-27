c51a76de8ad27b251f1fb3048b181f0e4b683649
Bodie Solomon <bsolomon@freewheel.tv>
2016-11-26 23:15:49 -0500

    Fix Convo bugfix name collision

--- rest/convo.go
+++ rest/convo.go
@@ -582,7 +582,7 @@ func (c Convo) Delete(w http.ResponseWriter, r *http.Request, ps htr.Params) {
 		return
 	}
 
-	if err := c.Update(convo.Delete([]byte(id))); err != nil {
+	if err := c.DB.Update(convo.Delete([]byte(id))); err != nil {
 		http.Error(w, fmt.Sprintf(
 			"failed to delete convo %#q: %s",
 			id, err.Error(),
