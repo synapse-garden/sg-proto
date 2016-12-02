e449d28ebacd001b6b7b206947811fac855757ac
Bodie Solomon <bsolomon@freewheel.tv>
2016-12-02 15:46:16 -0500

    Add more testing helpers for REST logins

--- testing/login.go
+++ testing/login.go
@@ -4,10 +4,36 @@ import (
 	"time"
 
 	"github.com/synapse-garden/sg-proto/auth"
+	"github.com/synapse-garden/sg-proto/incept"
+	"github.com/synapse-garden/sg-proto/users"
 
 	"github.com/boltdb/bolt"
+	"github.com/pkg/errors"
+	uuid "github.com/satori/go.uuid"
 )
 
+// MakeLogin creates a new auth.Login in the DB using the given name and
+// SHA256-hashed password.
+func MakeLogin(name, pw string, db *bolt.DB) (*auth.Login, error) {
+	tick := incept.Ticket(uuid.NewV4())
+	if err := db.Update(incept.NewTickets(tick)); err != nil {
+		return nil, errors.Wrapf(err, "failed to create ticket for user %#q", name)
+	}
+
+	user := &auth.Login{
+		User:   users.User{Name: name},
+		PWHash: Sha256(pw),
+	}
+
+	if err := incept.Incept(tick, user, db); err != nil {
+		return nil, errors.Wrapf(err, "failed to incept user %#q", name)
+	}
+
+	return user, nil
+}
+
+// GetSession creates a new Session in the DB for the given User, if the
+// user has a valid login.
 func GetSession(userID string, sesh *auth.Session, db *bolt.DB) error {
 	return db.Update(auth.NewSession(
 		sesh,
--- testing/util.go
+++ testing/util.go
@@ -1,6 +1,9 @@
 package testing
 
-import "time"
+import (
+	"crypto/sha256"
+	"time"
+)
 
 // Timeout constants.
 const (
@@ -17,4 +20,10 @@ type Timer time.Time
 // Now implements Timer.Now on Timer.
 func (t Timer) Now() time.Time {
 	return time.Time(t)
+}
+
+// Sha256 gets the SHA256 of the given string and slices the array.
+func Sha256(str string) []byte {
+	sum := sha256.Sum256([]byte(str))
+	return sum[:]
 }
