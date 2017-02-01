6f79a2c94f5bfaaf028c41426cc06e20ed0ad369
Bodie Solomon <bsolomon@freewheel.tv>
2017-02-01 18:37:55 -0500

    Update Task test harness

--- task/common_test.go
+++ task/common_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/synapse-garden/sg-proto/task"
 	sgt "github.com/synapse-garden/sg-proto/testing"
 	"github.com/synapse-garden/sg-proto/text"
+	"github.com/synapse-garden/sg-proto/users"
 
 	"github.com/boltdb/bolt"
 	. "gopkg.in/check.v1"
@@ -24,12 +25,15 @@ var _ = Suite(&TaskSuite{})
 func Test(t *testing.T) { TestingT(t) }
 
 func (s *TaskSuite) SetUpTest(c *C) {
-	db, tmpDir, err := sgt.TempDB("sg-stream-test")
+	db, tmpDir, err := sgt.TempDB("sg-task-test")
 	c.Assert(err, IsNil)
 	c.Assert(db.Update(store.Wrap(
 		store.Migrate(store.VerCurrent),
-		store.SetupBuckets(task.TaskBucket),
-		store.SetupBuckets(text.TextBucket),
+		store.SetupBuckets(
+			users.UserBucket,
+			task.TaskBucket,
+			text.TextBucket,
+		),
 	)), IsNil)
 	s.DB, s.tmpDir = db, tmpDir
 }
