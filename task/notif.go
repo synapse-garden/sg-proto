0bd5a2ba66bae4b2ab017b01e16ec3ce4154d383
Bodie Solomon <bsolomon@freewheel.tv>
2017-02-01 18:37:44 -0500

    Add task notif Resourcer for removal

--- task/notif.go
+++ task/notif.go
@@ -10,3 +10,10 @@ type Deleted string
 
 // Resource implements Resourcer.Resource on Deleted.
 func (Deleted) Resource() store.Resource { return "task-deleted" }
+
+// Removed is a Resourcer which can be used to notify that the user has
+// been removed from the Task without showing them the Task itself.
+type Removed ID
+
+// Resource implements Resourcer on Removed.
+func (Removed) Resource() store.Resource { return "task-removed" }
